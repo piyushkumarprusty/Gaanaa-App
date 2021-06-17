c95b6a96e17c48cb4fdf090c095f9ed215ea68ff
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-17 11:26:11 +0530

    Added permission given by user

---
+++ app/src/main/java/com/example/android/gaanaa/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.android.gaanaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // Dexter is an Android library that simplifies the process of requesting permissions at runtime.
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        Toast.makeText(MainActivity.this,"RunTIme perission given" , Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                        // To Ask Permission at runtime again
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check()  ;
+    }
+}
